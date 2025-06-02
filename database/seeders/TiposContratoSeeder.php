614d37a9b1f8ac2b687f09227b0c99d95b7d3016
Javier161362 <javier.jair.soplapuco.herrera@gmail.com>
2025-06-01 22:19:40 -0500

    TiposContratoSeeder

---
+++ database/seeders/TiposContratoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ContractType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TiposContratoSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $contract_types = [
+            ['name' => 'Nombrado'],
+            ['name' => 'Permanente'],
+            ['name' => 'Temporal'],
+        ];
+
+        DB::table('contract_types')->insert($contract_types);
+    }
+}
