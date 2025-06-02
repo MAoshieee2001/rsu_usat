573c74ddc45730c0d349aa087648183b41c72bac
Moises Fernando <moisesusat2001@gmail.com>
2025-06-01 22:31:03 -0500

    create_vacation_changes_migration

--- database/seeders/TiposContratoSeeder.php
+++ database/seeders/TiposContratoSeeder.php
@@ -3,12 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\ContractType;
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TiposContratoSeeder extends Seeder
 {
-
     public function run(): void
     {
         $contract_types = [
