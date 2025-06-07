59e57d0baceb2ecbf80dcf73ed3730a1b7a8d5f7
Moises Fernando <moisesusat2001@gmail.com>
2025-06-06 20:33:39 -0500

    xxx

--- app/Http/Controllers/admin/EmployeeController.php
+++ app/Http/Controllers/admin/EmployeeController.php
@@ -29,7 +29,6 @@ class EmployeeController extends Controller
             'employees.email',
             'employees.photo',
             'employees.phone',
-            'employees.password',
             'employees.status',
             't.name as type_name',
             'employees.created_at',
